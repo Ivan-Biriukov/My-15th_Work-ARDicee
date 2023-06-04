b48cef032ef792c9855f5f59b829a08686a10749
Ivan Biriukov <Grottt0@mail.ru>
2023-06-04 18:19:41 +0300

    touches started

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -58,6 +58,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            if results.isEmpty {
+                print("touched the plane")
+            } else {
+                print("touched somwhere else")
+            }
+        }
+    }
 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor {
